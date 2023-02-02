9db10fda6c54359fd5d07de285ff9c35937d49f7
Anildo Junior Carneiro <anildo.10@hotmail.com>
2023-02-02 09:05:39 -0300

    Adiciona a funcao exibirDespesas

--- src/Classes/DiarioMotorista.php
+++ src/Classes/DiarioMotorista.php
@@ -1,16 +1,21 @@
 <?php
 namespace DiarioMotorista;
 
-use DateTime;
-
 class DiarioMotorista{
     private \PDO $bancoDados;
     public function __construct(\PDO $bancoDados){
         $this->bancoDados = $bancoDados;
     }
     public function adicionarDespesa(OrigemDespesas $origem, int $valor){
-        $sql = "INSERT INTO despesas (despesa_quantidade, despesa_origem, despesa_data) VALUE ('{$valor}', '{$origem->value}', NOW())";
+        $sql = "INSERT INTO despesas (despesa_quantidade, despesa_origem, despesa_data)
+                               VALUE ('{$valor}', '{$origem->value}', NOW())";
+                               
         $this->bancoDados->exec($sql) or die(print_r($this->bancoDados->errorInfo(), true));
+    }
+    public function exibirDespesas(){
+        $sql = "SELECT * FROM despesas";
+        $despesas = $this->bancoDados->query($sql, \PDO::FETCH_ASSOC);
+        return $despesas->fetchAll();
     }
 }
 ?>
