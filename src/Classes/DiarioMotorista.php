d56f0affd9889fcfa2470bd1806a498841a025f3
Anildo Junior Carneiro <anildo.10@hotmail.com>
2023-02-02 08:15:08 -0300

    Adiciona funcao adicionarDespesa

--- src/Classes/DiarioMotorista.php
+++ src/Classes/DiarioMotorista.php
@@ -1,10 +1,16 @@
 <?php
 namespace DiarioMotorista;
 
+use DateTime;
+
 class DiarioMotorista{
     private \PDO $bancoDados;
     public function __construct(\PDO $bancoDados){
         $this->bancoDados = $bancoDados;
+    }
+    public function adicionarDespesa(OrigemDespesas $origem, int $valor){
+        $sql = "INSERT INTO despesas (despesa_quantidade, despesa_origem, despesa_data) VALUE ('{$valor}', '{$origem->value}', NOW())";
+        $this->bancoDados->exec($sql) or die(print_r($this->bancoDados->errorInfo(), true));
     }
 }
 ?>
