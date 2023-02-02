5666d5d91fe4cea708de9543c8bf165712b936a6
Anildo Junior Carneiro <anildo.10@hotmail.com>
2023-02-02 06:43:01 -0300

    Atualiza script sql para tabela receitas

---
+++ sql/DiarioMotorista_Receitas.sql
@@ -0,0 +1,10 @@
+-- Active: 1675304380784@@127.0.0.1@3306
+CREATE DATABASE DiarioMotorista;
+USE DiarioMotorista;
+CREATE TABLE receitas (
+        receita_id INT AUTO_INCREMENT NOT NULL,
+        receita_quantidade INT (3) NOT NULL,
+        receita_origem VARCHAR (10) NOT NULL,
+        receita_data DATE NOT NULL,
+        PRIMARY KEY(receita_id)
+);
